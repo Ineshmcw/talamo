60191056a78c0209b909d8140d48ef1dc6db7188
IneshMcw <inesh.murugan@multicorewareinc.com>
2024-08-23 15:49:27 +0530

    Added condition for python intrepreter

--- whl_installation.sh
+++ whl_installation.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 # Check if folder path and .whl file are provided
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: $0 <PROJECT_DIR> <path_to_whl_file>"
+if [ -z "$1" ]; then
+    echo "Usage: $0 <PROJECT_DIR>"
     exit 1
 fi
 
 PROJECT_DIR=$1
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 WHL_FILE="${SCRIPT_DIR}/wheel_files/talamo-2.0.0a2-cp310-cp310-linux_x86_64.whl"
 FOLDER_NAME=$(basename "$PROJECT_DIR")
-VENV_DIR="${PROJECT_DIR}/${FOLDER_NAME}_venv"
+VENV_NAME="${FOLDER_NAME}_venv"
+VENV_DIR="${PROJECT_DIR}/${VENV_NAME}"
+INTERPRETER_PATH="$PROJECT_DIR/$VENV_NAME/bin/python"
+VSCODE_SETTINGS_DIR="$PROJECT_DIR/.vscode"
+SETTINGS_FILE="$VSCODE_SETTINGS_DIR/settings.json"
 
 # Create the folder if it doesn't exist
 if [ ! -d "$PROJECT_DIR" ]; then
@@ -21,6 +26,14 @@ if [ ! -d "$VENV_DIR" ]; then
     python3 -m venv "$VENV_DIR"
     echo "Virtual environment created in $VENV_DIR"
 fi
+
+mkdir -p "$VSCODE_SETTINGS_DIR"
+
+echo '{
+  "python.pythonPath": "'"$INTERPRETER_PATH"'"
+}' > "$SETTINGS_FILE"
+
+echo "VSCode settings updated with Python interpreter: $INTERPRETER_PATH"
 
 chmod +x "${VENV_DIR}/bin/activate"
 
@@ -37,30 +50,27 @@ else
     exit 1
 fi
 
-activation = "
-'function ${FOLDER_NAME}_venv() {
+# Check if the function is already present in .bashrc
+if ! grep -q "function cd" ~/.bashrc; then
+  echo 'function cd() {
   builtin cd "$@"
-
   if [[ -z "$VIRTUAL_ENV" ]] ; then
-    if [[ -d ./${FOLDER_NAME}_venv ]] ; then
-      source ./${FOLDER_NAME}_venv/bin/activate
-    fi
+      if [[ "$PWD" == "/home/inesh/Innatera/python/test" ]] ; then
+        source ./test_venv/bin/activate
+      fi
   else
-    parentdir="$(dirname "$VIRTUAL_ENV")"
-    if [[ "$PWD"/ != "$parentdir"/* ]] ; then
-      deactivate
-    fi
+      parentdir="$(dirname "$VIRTUAL_ENV")"
+      if [[ "$PWD"/ != "$parentdir"/* ]] ; then
+        deactivate
+      fi
   fi
-}' >> ~/.bashrc
-"
+  }' >> ~/.bashrc
 
-
-if ! grep -q "function ${FOLDER_NAME}_venv" ~/.bashrc; then
-  echo "$activation" >> ~/.bashrc
   echo "Activation function added to .bashrc."
 else
   echo "Activation function already present in .bashrc."
 fi
 
+source ~/.bashrc
 
 echo "Setup complete. The virtual environment will auto-activate when you enter the $FOLDER_NAME directory."
